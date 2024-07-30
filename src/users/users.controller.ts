498d64a6b875543dd58767e10faa3f19bf8be1cc
Colin-nwk <collinsnwoko05@gmail.com>
2024-07-30 22:08:02 +0100

    CRUD controller

---
+++ src/users/users.controller.ts
@@ -0,0 +1,43 @@
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+  Query,
+} from '@nestjs/common';
+
+@Controller('users')
+export class UsersController {
+  @Get() //Get /users or /users?role=value
+  findAll(@Query('role') role?: 'INTERN'|'ADMIN'|'ENGINEER') {
+    return [];
+  }
+
+  //   @Get('interns') //Get /users/interns
+  //   findAllInterns() {
+  //     return [];
+  //   }
+
+  @Get(':id') //Get /users/:id
+  findOne(@Param('id') id: string) {
+    return { id };
+  }
+
+  @Post()
+  create(@Body() user: {}) {
+    return user;
+  }
+
+  @Patch(':id') //Patch /users/:id
+  update(@Param('id') id: string, @Body() userUpdate: {}) {
+    return { id, ...userUpdate };
+  }
+
+  @Delete(':id') //Get /users/:id
+  delete(@Param('id') id: string) {
+    return { id };
+  }
+}
